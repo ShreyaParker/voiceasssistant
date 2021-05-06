ecd6fd0db542c383d083a86e7aa9fe4bef826aea
Shreya <shreyaparker317@gmail.com>
2021-05-06 15:22:21 +0530

    calling function added

--- app/src/main/java/com/example/myapplication/Function.java
+++ app/src/main/java/com/example/myapplication/Function.java
@@ -1,24 +1,55 @@
 package com.example.myapplication;
 
+import android.util.Log;
+
 import java.util.Calendar;
 
 public class Function {
-    public static String wishMe(){
-        String s ="";
+    public static String wishMe() {
+        String s = "";
         Calendar c = Calendar.getInstance();
         int time = c.get(Calendar.HOUR_OF_DAY);
 
-        if(time >= 0 && time < 12){
+        if (time >= 0 && time < 12) {
             s = "Good Morning";
-        }else if(time >= 12 && time < 16){
+        } else if (time >= 12 && time < 16) {
             s = "Good Afternoon";
-        }else if(time >= 16 && time < 21){
+        } else if (time >= 16 && time < 21) {
             s = "Good Evening";
-        }else if(time >= 21 && time < 22){
+        } else if (time >= 21 && time < 22) {
             s = "Good Night";
-        }else if(time >= 22 && time < 24){
+        } else if (time >= 22 && time < 24) {
             s = "You need to take a rest... it's too late";
         }
         return s;
+    }
+
+    public static String fetchName(String msgs) {
+        String name = "";
+        boolean flag = false;
+        String[] data = msgs.split(" ");
+        for (int i = 0; i < data.length; i++) {
+            String d = data[i];
+            if (d.equals("call")) {
+                if (data[(i+1)].equals("to")){
+                    flag = false;
+                } else {flag = true;}
+            } else if(d.equals("and") || d.equals(".")){
+                flag = false;
+            } else if (data[(i-1)].equals("call")){
+                if (d.equals("to")){
+                    flag = true;
+                }
+            }
+
+            if (flag){
+                if(!d.equals("call") && !d.equals("to")){
+                    name = name.concat(" "+d);
+                }
+            }
+        }
+        Log.d("Name",name);
+
+        return name;
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,8 +9,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.icu.text.DateFormat;
-import android.icu.util.Calendar;
+import android.media.session.MediaSession;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -19,17 +18,36 @@ import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
+import android.text.format.DateFormat;
 import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ai_webza_tec.ai_method;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 
 import static android.Manifest.permission.RECORD_AUDIO;
+import static com.example.ai_webza_tec.ai_method.checkForPreviousCallList;
+import static com.example.ai_webza_tec.ai_method.clearContactListSavedData;
+import static com.example.ai_webza_tec.ai_method.getContactList;
+import static com.example.ai_webza_tec.ai_method.makeCall;
+import static com.example.ai_webza_tec.ai_method.makeCallFromSavedContactList;
+import static com.example.myapplication.Function.fetchName;
 import static com.example.myapplication.Function.wishMe;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
     private SpeechRecognizer speechRecognizer;
     private Intent intentRecognizer;
     private TextView textView;
+    private TextView textview2;
     private TextToSpeech tts;
 
 
@@ -49,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
         initializeTextToSpeech();
         initializeResult();
 
+
+
         ActivityCompat.requestPermissions(this, new String[]{RECORD_AUDIO}, PackageManager.PERMISSION_GRANTED); //To grant permission to use microphone
 
 
@@ -60,12 +81,12 @@ public class MainActivity extends AppCompatActivity {
     private void initializeTextToSpeech() {
         tts= new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
-            public void onInit(int status) {
+            public void onInit(int status) {  //called to signal the completion of the TextToSpeech engine initialization.
                 if (tts.getEngines().size()==0) {
                     Toast.makeText(MainActivity.this, "Engine is not available", Toast.LENGTH_SHORT).show();
                 }else{
-                    String s = wishMe();
-                    speak("Hii I am Shreya..."+s);
+                    String s = wishMe();//wishMe function to greet by time
+                    speak("Hii I am ..."+s);
 
                 }
             }
@@ -73,6 +94,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void speak(String msg) {
+        textview2.setText(msg);
+        //// Check if we're running on Android 5.0 or higher
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             tts.speak(msg,TextToSpeech.QUEUE_FLUSH, null, null);
         }else{
@@ -82,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void findById(){
         textView = (TextView)findViewById(R.id.textView);
-
+        textview2= (TextView)findViewById(R.id.textView2);
     }
     private void initializeResult() {
         if (SpeechRecognizer.isRecognitionAvailable( this)){
@@ -92,39 +115,41 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onReadyForSpeech(Bundle params) {
 
-            }
+            }  //called when the endpointer is ready for the user to start speaking
 
             @Override
             public void onBeginningOfSpeech() {
 
-            }
+            } //the user has started speaking
 
             @Override
             public void onRmsChanged(float rmsdB) {
 
-            }
+            }  //the sound level in the audio stream has changed
 
             @Override
             public void onBufferReceived(byte[] buffer) {
 
-            }
+            }  //more sound has been recieved
 
             @Override
             public void onEndOfSpeech() {
 
-            }
+            }  //Called after the user stops speaking
 
             @Override
             public void onError(int error) {
 
-            }
+            }  //A network or recognition error occured
 
             @Override
             public void onResults(Bundle bundle) {
                 ArrayList<String> result = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
                 Toast.makeText(MainActivity.this, ""+result.get(0), Toast.LENGTH_SHORT).show();
                 textView.setText(result.get(0));
-                responce(result.get(0));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    responce(result.get(0));
+                } //Called when recognition results are ready
             }
 
             @Override
@@ -135,20 +160,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onEvent(int eventType, Bundle params) {
 
-            }
+            } //Reserved for adding future eevents
         });
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    private void responce(String msg) {
+    private void responce(String msg) { //responce we get from tts
         String msgs = msg.toLowerCase();
         if (msgs.indexOf("hi")!= -1){
-            speak( "hello sir ! how are you?");
+            speak( "hello ! how are you?");
 
         }
         if (msgs.indexOf("hello")!= -1){
-            speak( "hello sir ! how are you?");
+            speak( "hello ! how are you?");
 
         }else if (msgs.indexOf("i am not fine")!= -1){
             speak("please take care");
@@ -156,53 +181,89 @@ public class MainActivity extends AppCompatActivity {
         if (msgs.indexOf("what")!= -1){
             if (msgs.indexOf("your")!= -1){
                 if (msgs.indexOf("name")!= -1){
-                    speak( "My name is shreya");
-                }
+                    speak( "My name is ");
+                }//tells the name
             }
             if(msgs.indexOf("time")!= -1){
                 if(msgs.indexOf("now")!= -1){
                     Date date = new Date();
                     String time = DateUtils.formatDateTime(this,date.getTime(),DateUtils.FORMAT_SHOW_TIME);
                     speak("The time now is"+time);
-                }
+                }//tells currunt time
             }
 
             if(msgs.indexOf("today")!= -1){
                 if(msgs.indexOf("date")!= -1){
-                   SimpleDateFormat df = new SimpleDateFormat("dd MM yyyy");
-                   Calendar cal = Calendar.getInstance();
-                   String todays_date = df.format(cal.getTime());
-                   speak("the today's date is "+todays_date);
-                }
+                    SimpleDateFormat df = new  SimpleDateFormat("dd MMMM yyyy");
+                    Calendar cal = Calendar.getInstance();
+                    String todays_date = df.format(cal.getTime());
+                    speak("the today's date is "+todays_date);
+                }//tells currunt date
             }
         }
         if(msgs.indexOf("open")!= -1){
             if(msgs.indexOf("google")!= -1){
-                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));//to open youtube
                 startActivity(intent);
             }
-            if(msgs.indexOf("browser")!= -1){
-                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
-                startActivity(intent);
-            }
-            if(msgs.indexOf("chrome")!= -1){
-                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
-                startActivity(intent);
-            }
+
             if(msgs.indexOf("youtube")!= -1){
-                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com"));
+                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com"));//to open youtube
                 startActivity(intent);
             }
             if(msgs.indexOf("facebook")!= -1){
-                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com"));
+                Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com"));// To open faceboock webpage
                 startActivity(intent);
             }
             if(msgs.indexOf("whatsapp")!= -1){
                 Context ctx = this;
-                Intent i = ctx.getPackageManager().getLaunchIntentForPackage("com.whatsapp");
+                Intent i = ctx.getPackageManager().getLaunchIntentForPackage("com.whatsapp");//to open whatsapp
                 ctx.startActivity(i);
             }
         }
+
+        if(msgs.indexOf("call")!= -1){
+            final String[] listname ={""};
+            final String name = fetchName(msgs);
+            Log.d("Name",name);
+
+            Dexter.withContext(this)
+                    .withPermissions(
+                            Manifest.permission.READ_CONTACTS,
+                            Manifest.permission.CALL_PHONE
+                    ).withListener(new MultiplePermissionsListener() {
+                @Override
+                public void onPermissionsChecked(MultiplePermissionsReport report) {
+                    if (report.areAllPermissionsGranted()) {
+                        if (checkForPreviousCallList(MainActivity.this)) {
+                            speak(makeCallFromSavedContactList(MainActivity.this, name));
+                        } else {
+                            HashMap<String, String> list = getContactList(MainActivity.this, name);
+                            if (list.size() > 1) {
+                                for (String i : list.keySet()) {
+                                    listname[0] = listname[0].concat("..........................!" + i);
+                                }
+                                speak("Which one sir ? .. There is " + listname[0]);
+                            } else if (list.size() == 1) {
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                                    makeCall(MainActivity.this, list.values().stream().findFirst().get());
+                                    clearContactListSavedData(MainActivity.this);
+                                }
+                            } else {
+                                speak("NO CONTACT FOUND");
+                                clearContactListSavedData(MainActivity.this);
+                            }
+                        }
+                    }
+                    if (report.isAnyPermissionPermanentlyDenied()) {
+                    }
+                }
+                @Override public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                    token.continuePermissionRequest();}
+
+            }).check();
+        }
+
 
 
     }
@@ -217,3 +278,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
